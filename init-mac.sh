17622f14eff067f32c022b1ee692148516da6e8d
Jonathan Meier <jntme@mb-jntme.local>
2019-03-14 14:56:52 +0100

    add ideavim config

--- init-mac.sh
+++ init-mac.sh
@@ -24,6 +24,7 @@ mkdir -p ~/.config ~/.config/nvim
 ln -s ~/.dots/zshrc ~/.zshrc
 ln -s ~/.dots/tmux.conf ~/.tmux.conf
 ln -s ~/.dots/vimrc ~/.config/nvim/init.vim
+ln -s ~/.dots/ideavim ~/.ideavim
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
